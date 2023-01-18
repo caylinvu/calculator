b7dfb63bed0f3519292bad7be9514513a71ce96b
Caylin Vu <caylinvu@gmail.com>
2023-01-18 15:27:09 -0600

    Add basic functionality to operator buttons

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const displayText = document.querySelector('.display-text');
 const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('.equals');
 let displayValue = '0';
+let num1 = '';
+let num2 = '';
+let operator = '';
+let result = '';
+let isCompleteNumber = false;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -33,6 +40,11 @@ function operate(operator, num1, num2) {
 function populateNumbers() {
     numberButtons.forEach((button) => {
         button.addEventListener('click', function (e) {
+            if (isCompleteNumber) {
+                displayText.textContent = '';
+                displayValue = '0';
+                isCompleteNumber = false;
+            }
             if (displayValue.length < 14) {
                 displayText.textContent += e.target.textContent;
                 displayValue = displayText.textContent.toString();
@@ -42,3 +54,21 @@ function populateNumbers() {
 }
 
 populateNumbers();
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        operator = e.target.textContent;
+        num1 = displayValue;
+        isCompleteNumber = true;
+        console.log(operator);
+        console.log(num1);
+        
+        equalButton.addEventListener('click', function (e) {
+            num2 = displayValue;
+            console.log(num2);
+            result = operate(operator, num1, num2);
+            displayText.textContent = result;
+        });
+    });
+});
+
